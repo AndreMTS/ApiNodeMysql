a4d3bf3a18ecd7f70aa06f809d9c001e82489976
AndreMTS <andre.matos@sisloc.com.br>
2023-04-18 19:13:40 -0300

    #02 - criação de arquivos de rota e variavel de ambiente

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('funcionando')
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+require('dotenv').config();
+
+const protocol = process.env.PROTOCOL || "http"
+const ip = require('ip').address()
+const port = process.env.PORT || 3030
+
+app.use(routes)
+
+app.listen(port, ()=> console.log(`
+    Server started in http://localhost:${port} or ${protocol}://${ip}:${port}
+`));
